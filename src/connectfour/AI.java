abe67c6d5d55dda0d1381dbb2f06301504f32004
Jordan Wine <jordandwine@gmail.com>
2016-09-27 12:21:35 -0700

    Player Changes
    Updated Player class and subclasses. Minor edits to GameEngine methods
    for validation purposes

--- src/connectfour/AI.java
+++ src/connectfour/AI.java
@@ -1,8 +1,42 @@
 package connectfour;
+import java.util.*;
 
 public class AI extends Player{
+	
+	public AI(){
+		super(2,"Computer",'X');
+	}
+	
+	public AI(char pieceType){
+		super(2,"Computer",pieceType);
+	}
+	
+	public AI(String name,char pieceType){
+		super(2,name,pieceType);
+	}
 
-	public void placePiece() {
-		//Override placePiece function to work out some sort of logic the PC can use to determine where to place a piece
+	public void placePiece(GameEngine game) {
+		// Start with a list of all columns
+		ArrayList<Integer> columns = new ArrayList<Integer>(
+				Arrays.asList(0,1,2,3,4,5,6));
+		boolean success;
+		do{
+			// Pick a random column
+			int col = getRandom(columns);
+			// Try putting a piece in that column. If it doesn't work, remove 
+			// that column from possibilities and pick a different column.
+			success = game.placePiece(col, pieceType);
+			if (! success){
+				columns.remove(col);
+			}
+			// if successful add that pick to history
+			else
+				history.add(col);
+		} while (! success);
+	}
+	
+	private int getRandom(ArrayList<Integer> array){
+		int rand = new Random().nextInt(array.size());
+		return array.get(rand);
 	}
 }
---
+++ src/connectfour/Connect4Game.java
@@ -0,0 +1,24 @@
+package connectfour;
+
+public class Connect4Game {
+
+	public static void main(String[] args) {
+		/**
+		Instantiate a game and player1
+		prompt player one for game mode (multiplayer or vs. AI)
+		If AI, instantiate AI
+		If multiplayer, instantiate player2
+		Start with some player
+		While no one has won
+			player takes an action
+			game checks for win condition
+			if win condition is met
+				display winner
+				end game
+			else
+				change to next player
+		*/
+
+	}
+
+}
